d9f2aaa6925620bbcf4f0b8b6b50166ab84c3ce8
LadyBug-jay <judithelemo@gmail.com>
2022-09-28 20:17:27 +0100

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -5,7 +5,11 @@ public class class03 {
 	public static void main(String[] args) {
 		
 		
-		System.out.print("hey this is a git class");
+		System.out.println("hey this is a git class");
+		System.out.println("hey this is a change in my project");
+		
+		
+		
 	}
 	
 
